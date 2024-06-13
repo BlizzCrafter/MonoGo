2a647085b856dc1caa57853e0223152f3eec4231
BlizzCrafter <m.haertel@gmx.net>
2024-06-13 20:22:01 +0200

    Finished FollowPositionModifier.

--- MonoGo.Engine/Particles/Modifiers/FollowPositionModifier.cs
+++ MonoGo.Engine/Particles/Modifiers/FollowPositionModifier.cs
@@ -11,7 +11,7 @@ namespace MonoGo.Engine.Particles.Modifiers
         public IMovable Followable { get; set; }
         public Vector2 Offset { get; set; }
         public float Speed { get; set; } = 1f;
-        public bool Inside { get; set; }
+        public bool Inside { get; set; } = true;
 
         public unsafe void Update(float elapsedSeconds, ParticleBuffer.ParticleIterator iterator)
         {
@@ -22,8 +22,27 @@ namespace MonoGo.Engine.Particles.Modifiers
                 if (Followable != null)
                 {
                     var angle = new Angle(position, Followable.Position + Offset);
-                    if (Inside) angle *= -1;
+                    if (!Inside) angle *= -1;
                     position += (Vector2)angle * Speed * elapsedSeconds;
+                }
+                else
+                {
+                    var angle = new Angle(position, particle->TriggerPos + Offset);
+                    if (!Inside) angle *= -1;
+                    var distance = position - particle->TriggerPos + Offset;
+                    var length = distance.Length();
+                    if (length > 100f)
+                    {
+                        position += (Vector2)angle * Speed * elapsedSeconds;
+                    }
+                    else if (length > 1f)
+                    {
+                        var angle2 = new Angle(position, particle->TriggerPos);
+                        var distance2 = position - particle->TriggerPos;
+                        var length2 = distance.Length();
+                        position += (Vector2)angle * Speed * elapsedSeconds;
+                    }
+                    else position = particle->TriggerPos;
                 }
                 particle->Position = position;
             }
