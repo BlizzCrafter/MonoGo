65f2aae6568d23aa7d889205ca6301545e2a8622
BlizzCrafter <m.haertel@gmx.net>
2024-06-21 17:00:11 +0200

    Added extension for taking the next or the previous enum.

---
+++ MonoGo.Engine/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MonoGo.Engine
+{
+    public static class Extensions
+    {
+        public static T Next<T>(this T currentValue) where T : Enum
+        {
+            T[] values = (T[])Enum.GetValues(typeof(T));
+            int index = Array.IndexOf(values, currentValue);
+
+            if (index == values.Length - 1)
+            {
+                return values[0];
+            }
+
+            return values[index + 1];
+        }
+
+        public static T Previous<T>(this T currentValue) where T : Enum
+        {
+            T[] values = (T[])Enum.GetValues(typeof(T));
+            int index = Array.IndexOf(values, currentValue);
+
+            if (index == 0)
+            {
+                return values[^1];
+            }
+
+            return values[index - 1];
+        }
+    }
+}
