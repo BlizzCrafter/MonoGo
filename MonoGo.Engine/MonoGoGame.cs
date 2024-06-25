2c3d74daf1537af834f6ea2975eb187eb90791bf
BlizzCrafter <m.haertel@gmx.net>
2024-06-25 12:49:52 +0200

    Add: Abstract MonoGoGame class for internal init, update, draw and dispose logic so the user doesn't need to care about this stuff.

---
+++ MonoGo.Engine/MonoGoGame.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGo.Engine.Drawing;
+using MonoGo.Engine.Resources;
+using MonoGo.Engine.UI;
+using MonoGo.Resources;
+using System;
+
+namespace MonoGo.Engine
+{
+    public abstract class MonoGoGame : Game
+    {
+        public MonoGoGame()
+        {
+            Content.RootDirectory = ResourceInfoMgr.ContentDir;
+            GameMgr.Init(this);
+
+            if (GameMgr.CurrentPlatform == Platform.Android)
+            {
+                GameMgr.WindowManager.SetFullScreen(true); // Has to be exactly here, apparently.
+            }
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            RenderMgr.Init();
+
+            var depth = new DepthStencilState
+            {
+                DepthBufferEnable = true,
+                DepthBufferWriteEnable = true
+            };
+            GraphicsMgr.Device.PresentationParameters.DepthStencilFormat = DepthFormat.Depth24Stencil8;
+
+            GraphicsMgr.VertexBatch.DepthStencilState = depth;
+        }
+
+        protected override void LoadContent()
+        {
+            GraphicsMgr.Init(GraphicsDevice);
+
+            new SpriteGroupResourceBox("GUISprites", "GUI");
+            new SpriteGroupResourceBox("ParticleSprites", "Particles");
+            new SpriteGroupResourceBox("LUTSprites", "LUT");
+            new DirectoryResourceBox<Effect>("Effects", "Effects");
+            new FontResourceBox("Fonts", "Fonts");
+
+            UserInterface.Init("GUI/Styles");
+            UserInterface.Active.BlendState = BlendState.AlphaBlend;
+            UserInterface.Active.SamplerState = SamplerState.PointWrap;
+        }
+
+        protected override void UnloadContent()
+        {
+            ResourceHub.UnloadAll();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            GameMgr.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GameMgr.Draw(gameTime);
+
+            UserInterface.Active.DrawCursor();
+
+            base.Draw(gameTime);
+        }
+
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            RenderMgr.Dispose();
+
+            base.OnExiting(sender, args);
+        }
+    }
+}
--- Samples/MonoGo.Samples/Game1.cs
+++ Samples/MonoGo.Samples/Game1.cs
@@ -1,33 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using MonoGo.Engine;
 using MonoGo.Engine.Drawing;
 using MonoGo.Engine.Resources;
-using MonoGo.Engine.UI;
 using MonoGo.Resources;
-using MonoGo.Samples.Resources;
 using MonoGo.Tiled;
 using MonoGo.Tiled.MapStructure;
-using System;
 
 namespace MonoGo.Samples
 {
-	/// <summary>
-	/// This is the main type for your game.
-	/// </summary>
-	public class Game1 : Game
+    /// <summary>
+    /// This is the main type for your game.
+	/// Note: You need to inherit from the MonoGoGame class!
+    /// </summary>
+    public class Game1 : MonoGoGame
     {
-        public Game1()
-		{
-			Content.RootDirectory = ResourceInfoMgr.ContentDir;
-            GameMgr.Init(this);
-
-            if (GameMgr.CurrentPlatform == Platform.Android)
-			{
-				GameMgr.WindowManager.SetFullScreen(true); // Has to be exactly here, apparently.
-			}
-        }
-
 		/// <summary>
 		/// Allows the game to perform any initialization it needs to before starting to run.
 		/// This is where it can query for any required services and load any non-graphic
@@ -38,15 +24,7 @@ namespace MonoGo.Samples
         {
 			base.Initialize();
 
-            RenderMgr.Init();
             TiledEntityFactoryPool.InitFactoryPool();
-
-            var depth = new DepthStencilState();
-			depth.DepthBufferEnable = true;
-			depth.DepthBufferWriteEnable = true;
-			GraphicsMgr.Device.PresentationParameters.DepthStencilFormat = DepthFormat.Depth24Stencil8;
-
-			GraphicsMgr.VertexBatch.DepthStencilState = depth;
             new GameController();
         }
 
@@ -56,30 +34,15 @@ namespace MonoGo.Samples
 		/// </summary>
 		protected override void LoadContent()
         {
-            GraphicsMgr.Init(GraphicsDevice);
+			base.LoadContent();
 
+            new DirectoryResourceBox<TiledMap>("Maps", "Maps");
             new SpriteGroupResourceBox("DemoSprites", "Demo");
-            new SpriteGroupResourceBox("GUISprites", "GUI");
-            new SpriteGroupResourceBox("ParticleSprites", "Particles");
-            new SpriteGroupResourceBox("LUTSprites", "LUT");
-            new DirectoryResourceBox<Effect>("Effects", "Effects");
-			new DirectoryResourceBox<TiledMap>("Maps", "Maps");
 
-			new Fonts();
-
-            UserInterface.Init("GUI/Styles");
-            UserInterface.Active.BlendState = BlendState.AlphaBlend;
-            UserInterface.Active.SamplerState = SamplerState.PointWrap;
+            var fontSprite = ResourceHub.GetResource<Sprite>("DemoSprites", "Font");
+            var fontBox = ResourceHub.GetResourceBox("Fonts") as ResourceBox<IFont>;
+            fontBox.AddResource("FancyFont", new TextureFont(fontSprite, 1, 1, TextureFont.Ascii, false));
         }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-		{
-			ResourceHub.UnloadAll();
-		}
 
 		/// <summary>
 		/// Allows the game to run logic such as updating the world,
@@ -87,10 +50,10 @@ namespace MonoGo.Samples
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
-		{
-			GameMgr.Update(gameTime);
-			
+		{			
 			base.Update(gameTime);
+
+			//You update logic here!
 		}
 
 		/// <summary>
@@ -99,18 +62,9 @@ namespace MonoGo.Samples
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
         {
-            GameMgr.Draw(gameTime);
-
-			UserInterface.Active.DrawCursor();
-
 			base.Draw(gameTime);
-        }
 
-        protected override void OnExiting(object sender, EventArgs args)
-        {
-			RenderMgr.Dispose();
-
-            base.OnExiting(sender, args);
+            //You draw logic here!
         }
     }
 }
